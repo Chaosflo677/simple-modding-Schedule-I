e47136788eb5c1546cbb8fb23093cc15abfa98a6
Chaosflo677 <florian.t.winter@gmail.com>
2025-12-02 22:43:44 +0100

    Projektdateien hinzufügen.

---
+++ simple modding/Class1.cs
@@ -0,0 +1,38 @@
+﻿using Il2CppScheduleOne.Interaction;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+namespace simple_modding
+{
+    public class Class1:MelonPlugin
+    {
+        public override void OnInitializeMelon()
+        {
+            MelonLogger.Msg("Simple Modding Loaded");
+        }
+        internal class Interaction : MelonPlugin
+        {
+            public static void New_interaction(string Gameobject, string message, Action InteractedWith)
+            {
+                GameObject gameobject;
+                InteractableObject gameobjectInteraction;
+                gameobject = GameObject.Find(Gameobject);
+                gameobjectInteraction = gameobject.AddComponent<InteractableObject>();
+                gameobjectInteraction.message = message;
+                gameobjectInteraction.interactionState = InteractableObject.EInteractableState.Default;
+                void CallInteraction() => InteractedWith();
+                gameobjectInteraction.onInteractStart.AddListener((UnityAction)CallInteraction);
+            }
+            public static void New_label(string Gameobject, string message)
+            {
+                GameObject gameobject;
+                InteractableObject gameobjectInteraction;
+                gameobject = GameObject.Find(Gameobject);
+                gameobjectInteraction = gameobject.AddComponent<InteractableObject>();
+                gameobjectInteraction.message = message;
+                gameobjectInteraction.interactionState = InteractableObject.EInteractableState.Label;
+            }
+        }
+    }
+}
---
+++ simple modding/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using MelonLoader;
+using simple_modding;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+
+[assembly: MelonInfo(typeof(Class1), "simple modding", "1.0.0", "Chaosflo677")]
+[assembly: MelonGame("TVGS", "Schedule I")]
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("simple modding")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("simple modding")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("4410ba1f-102c-4f20-bbbc-8784bfd37d2b")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
